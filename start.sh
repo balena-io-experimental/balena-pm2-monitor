d0de29792a63d0d9266bd3816bfd80ad96cab924
gaudi <craig@resin.io>
2016-06-30 12:09:13 +0100

    correct keymetrics key ordering

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Link pm2 to keymetrics API and name instance resin DEVICE ID
-pm2 link $PUBLIC_KEY $SECRET_KEY $RESIN_DEVICE_UUID
+pm2 link $SECRET_KEY $PUBLIC_KEY $RESIN_DEVICE_UUID
 # Start pm2 process to run server.js forever
 pm2 start /app/server.js
 # Spit out some logs
